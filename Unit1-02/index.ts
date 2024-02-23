a9ecca3b8ebb987d8cfa7be4f2f280bfce5cd1c6
Infinity de Guzman <infinity.deguzman@mths.ca>
2024-02-23 10:03:40 -0500

    trying java

--- Unit1-02/index.ts
+++ Unit1-02/index.ts
@@ -17,9 +17,9 @@ const massFloat = parseFloat(mass.value || "-1")
 let energy = massFloat * (light ** 2)
 
 if (isNaN(massFloat) || massFloat < 0) {
-        console.log("Invalid input.")
+  console.log("Invalid input.")
 } else {
-        console.log(`${massFloat} kg of mass would produce ${energy.toExponential(3)} of energy.`)
+  console.log(`${massFloat} kg of mass would produce ${energy.toExponential(3)} of energy.`)
 }
 
 console.log("\nDone.")
