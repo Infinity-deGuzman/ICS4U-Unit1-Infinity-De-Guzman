f8d65f6a71702e8d3e6ffeedb0b998ebf20d41c0
Infinity de Guzman <infinity.deguzman@mths.ca>
2024-02-23 09:34:40 -0500

    save

--- Unit1-02/index.ts
+++ Unit1-02/index.ts
@@ -8,7 +8,18 @@
 
 import {createPrompt} from 'bun-promptx'
 
+// input
 const mass = createPrompt('Enter the mass of an object in kilograms: ')
-const c = 2.998 * (10 ** 8)
-let energy = mass * (c ** 2)
+const light = 2.998e8
 
+// process & output
+const massFloat = parseFloat(mass.value || "-1")
+let energy = massFloat * (light ** 2)
+
+if (isNaN(massFloat) || massFloat < 0) {
+        console.log("Invalid input.")
+} else {
+        console.log(`${massFloat} kg of mass would produce ${energy.toExponential(3)} of energy.`)
+}
+
+console.log("\nDone.")
