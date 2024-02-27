f5687bd618f10783ef9704bf644753db435f3fff
Infinity <infinity.deguzman@mths.ca>
2024-02-27 14:38:27 +0000

    added J for units

--- Unit1-02/index.ts
+++ Unit1-02/index.ts
@@ -19,7 +19,7 @@ let energy = massFloat * (light ** 2)
 if (isNaN(massFloat) || massFloat < 0) {
   console.log("Invalid input.")
 } else {
-  console.log(`${massFloat} kg of mass would produce ${energy.toExponential(3)} of energy.`)
+  console.log(`${massFloat} kg of mass would produce ${energy.toExponential(3)}J of energy.`)
 }
 
 console.log("\nDone.")
