16b7ef2402eeb19f85b293c8e159de20c9a8408e
Infinity :0 <82898183+Infinity-deGuzman@users.noreply.github.com>
2024-03-28 09:51:22 -0400

    done index.ts

--- Unit1-04/index.ts
+++ Unit1-04/index.ts
@@ -9,8 +9,32 @@
 
 import {createSelection} from 'bun-promptx'
 
-let counter = 0
+// variables
 const randomNumber = Math.floor(Math.random() * 6) + 1
+let guessTotal = 0
 
-while (true) {
 
+while (1) {
+    // input
+    const guess = createPrompt("Choose a number between 1 and 6: ")
+    const guessInt = parseInt(guess.value || "-1")
+    // error if guess cannot be classified as a pos. integer,
+    // or not within parameters
+    if (isNaN(guessInt) == true || guessInt < 1 || userGuessInt > 6) {
+        console.log("Invalid input. Try again.\n")
+    } else {
+        // process
+        guessTotal += 1
+        if (guessInt > randomNumber) {
+            console.log(`You guessed too high, try again!\n`)
+        } else if (guessInt < randomNumber) {
+            console.log(`You guessed too low, try again!\n`)
+        } else {
+            // output
+            console.log(`Congrats! You got it in ${guessTotal} tries :)`)
+            break
+        }
+    }
+}
+
+console.log("\nDone.")
