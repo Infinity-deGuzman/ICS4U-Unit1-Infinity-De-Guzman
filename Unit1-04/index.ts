f906e3c5afc4fdb2cb015799b2c25cb1eff1a468
Infinity :0 <82898183+Infinity-deGuzman@users.noreply.github.com>
2024-04-19 01:43:45 +0000

    did ts

--- Unit1-04/index.ts
+++ Unit1-04/index.ts
@@ -1,40 +1,39 @@
-/**
- * This program generates a random number
- * for the player to guess.
+/*
+ * This is a program creates a random number 
+ * that the user tries to guess
  *
- * By: Infinity de Guzman
- * Version: 1.0
- * Since: 2024-02-27
+ * @author  Infinity de Guzman
+ * @version 1.0
+ * @since   2024-04-05
  */
 
-import {createSelection} from 'bun-promptx'
+import { createPrompt } from 'bun-promptx'
 
-// variables
-const randomNumber = Math.floor(Math.random() * 6) + 1
-let guessTotal = 0
-
+// constants & variables
+const randomNum = Math.floor((Math.random() * 8) + 1)
+let totalGuess = 0
 
 while (1) {
-    // input
-    const guess = createPrompt("Choose a number between 1 and 6: ")
-    const guessInt = parseInt(guess.value || "-1")
-    // error if guess cannot be classified as a pos. integer,
-    // or not within parameters
-    if (isNaN(guessInt) == true || guessInt < 1 || userGuessInt > 6) {
-        console.log("Invalid input. Try again.\n")
+  // input
+  const userGuess = createPrompt("Guess a number between 1 - 8: ")
+  const guessInt = parseInt(userGuess.value || "-1")
+  // error check
+  if (isNaN(guessInt) == true || guessInt < 1 || guessInt > 8) {
+    console.log("Invalid input. (This won't count as a guess)\n")
+  } else {
+    // process
+    totalGuess += 1
+    if (guessInt > randomNum) {
+      console.log(`You guessed too high :( try again.\n`)
+    } else if (guessInt < randomNum) {
+      console.log(`You guessed too low :( try again.\n`)
     } else {
-        // process
-        guessTotal += 1
-        if (guessInt > randomNumber) {
-            console.log(`You guessed too high, try again!\n`)
-        } else if (guessInt < randomNumber) {
-            console.log(`You guessed too low, try again!\n`)
-        } else {
-            // output
-            console.log(`Congrats! You got it in ${guessTotal} tries :)`)
-            break
-        }
+      // output
+      console.log(`Correct !!\nTotal guesses: ${totalGuess}`)
+      break
     }
+  }
 }
 
 console.log("\nDone.")
+
